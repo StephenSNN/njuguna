687d82a90a9c7dde19765da35a20c4755f844fc7
StephenSNN <162370976+StephenSNN@users.noreply.github.com>
2024-03-27 23:56:52 +0300

    Add files via upload

---
+++ StephenNjuguna.js
@@ -0,0 +1,29 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+undefined
+//Example usage:
+undefined
+const originalString = "Hello, world!";
+undefined
+const reversedString = reverseString(originalString);
+undefined
+console.log(reversedString); //Output:
+VM523:1 !dlrow ,olleH
+undefined
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+undefined
+//Example usage:
+undefined
+const originalString = "Hello";
+undefined
+const reversedString = reverseString(originalString);
+undefined
+const originalString = "12345";
+undefined
+const reversedString = reverseString(originalString);
+undefined
+console.log(reversedString); //Output:
+VM895:1 54321
